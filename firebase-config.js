d5154ca09b5b7ee7734b5f45c5324830f5f8e535
Tom Walsh <tomwalsh96@gmail.com>
2025-04-08 22:39:28 +0100

    forgot auth was pulling firebase api from .env, so reinstated

--- firebase-config.js
+++ firebase-config.js
@@ -6,13 +6,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCqQR6fCgUMH28LXmAvno_fFbVb7VzHz-k",
-    authDomain: "aura-6d880.firebaseapp.com",
-    projectId: "aura-6d880",
-    storageBucket: "aura-6d880.firebasestorage.app",
-    messagingSenderId: "268033800249",
-    appId: "1:268033800249:web:861c347b25819fdb6775e8",
-    measurementId: "G-WJTFWZ235N"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
